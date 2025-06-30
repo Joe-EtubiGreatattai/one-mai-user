96457a5888bc90c8d1a437d0f2394b70b589965b
Joe-EtubiGreatattai <greatattai442442@gmail.com>
2025-06-30 10:35:49 +0100

    push

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -178,7 +178,7 @@ const SignIn = () => {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
+                  
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
@@ -200,7 +200,7 @@ const SignIn = () => {
                     name="password"
                     type={showPassword ? "text" : "password"}
                     autoComplete="current-password"
-                    required
+                    
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     className="w-full px-3 py-2 sm:px-4 sm:py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 pr-10"
